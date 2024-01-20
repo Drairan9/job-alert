acf121ac18206f8bc9a4635ffd5b5a4cedc8f5a8
Drairan9 <drairan@protonmail.com>
2024-01-20 22:45:04 +0100

    feat(discord): Discord default

---
+++ src/core/discord/DiscordBot.ts
@@ -0,0 +1,24 @@
+import { Client, GatewayIntentBits } from 'discord.js';
+
+export default class DiscordBot extends Client {
+	private readonly BOT_TOKEN;
+
+	constructor(token: string) {
+		super({
+			intents: [
+				GatewayIntentBits.Guilds,
+				GatewayIntentBits.GuildMessages,
+				GatewayIntentBits.GuildVoiceStates,
+				GatewayIntentBits.GuildMessageReactions,
+				GatewayIntentBits.GuildMembers,
+				GatewayIntentBits.MessageContent,
+			]
+		});
+		this.BOT_TOKEN = token;
+		this.start();
+	}
+
+	private async start() {
+		await this.login(this.BOT_TOKEN);
+	}
+}
