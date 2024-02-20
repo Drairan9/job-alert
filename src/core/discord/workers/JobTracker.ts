ebd057271a36c4ea30cacedc96dc8024d11951e8
Drairan <drairan@protonmail.com>
2024-02-20 18:14:47 +0100

    one more catch

--- src/core/discord/workers/JobTracker.ts
+++ src/core/discord/workers/JobTracker.ts
@@ -12,33 +12,38 @@ export default class JobTracker {
 
 	private startWorker(getNewJobs: () => Promise<TGetNewJobs>) {
 		setInterval(async () => {
-			const user = await this.client.users.fetch(process.env.OWNER_ID!);
-			const newJobs = await getNewJobs();
-			if (newJobs.isError) {
-				return await user.send(`JobTracker ${newJobs.provider} exception: ${newJobs.errorText}`);
-			}
-			if (newJobs.jobs === null || newJobs.jobs.length <= 0) {
-				return await user.send(`JobTracker ${newJobs.provider} exception: Jobs array is null or empty.`);
-			}
-			await user.send('New job alert!');
+			try {
+				const user = await this.client.users.fetch(process.env.OWNER_ID!);
+				const newJobs = await getNewJobs();
+				if (newJobs.isError) {
+					return await user.send(`JobTracker ${newJobs.provider} exception: ${newJobs.errorText}`);
+				}
+				if (newJobs.jobs === null || newJobs.jobs.length <= 0) {
+					return await user.send(`JobTracker ${newJobs.provider} exception: Jobs array is null or empty.`);
+				}
+				await user.send('New job alert!');
 
-			newJobs.jobs.forEach((job: TJob) => {
-				const embed = new EmbedBuilder()
-					.setColor(0x68c70f)
-					.setTitle(job.title)
-					.setURL(job.url)
-					.setAuthor({name: job.company})
-					.setThumbnail(job.thumbnail)
-					.setFooter({text: job.website})
-					.addFields({
-						name: 'Salary',
-						value: job.salary
-					}, {
-						name: 'Tags',
-						value: job.tags.join(', ')
-					});
-				user.send({embeds: [embed]});
-			});
+				newJobs.jobs.forEach((job: TJob) => {
+					const embed = new EmbedBuilder()
+						.setColor(0x68c70f)
+						.setTitle(job.title)
+						.setURL(job.url)
+						.setAuthor({name: job.company})
+						.setThumbnail(job.thumbnail)
+						.setFooter({text: job.website})
+						.addFields({
+							name: 'Salary',
+							value: job.salary
+						}, {
+							name: 'Tags',
+							value: job.tags.join(', ')
+						});
+					user.send({embeds: [embed]});
+				});
+			} catch (e) {
+				const user = await this.client.users.fetch(process.env.OWNER_ID!);
+				user.send(`${e}`);
+			}
 
 		}, this.INTERVAL);
 	}
